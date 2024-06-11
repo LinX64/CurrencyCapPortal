3621b4692ad56add777e5cb1ac4e8b6d17c28f32
Abdolrahman Farshgar <ifarshgar@gmail.com>
2024-06-11 21:54:45 +0200

    Making progress with the APIs

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Flask, request, jsonify, Blueprint
+
 
 main = Blueprint('main', __name__)
 
@@ -7,3 +8,22 @@ main = Blueprint('main', __name__)
 def index():
     return "Hello, World!"
 
+
+@main.route('/get_currencies')
+def get_currencies():
+    
+    return 'Test'
+
+
+@main.route('/timeframe', methods=['POST'])
+def handle_post_timeframe():
+    data = request.json
+
+    timeframe = data.get('timeframe', '1H')
+
+    response = {
+        'message': 'Received POST request!',
+        'data': f'Received: {timeframe}'
+    }
+
+    return jsonify(response)
