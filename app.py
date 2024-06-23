e9f421a570c255165ff80ce5554d5cb8ac5ae26b
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-06-23 13:24:38 +0200

    Fixed issues

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import jsonify
 from app import create_app
 from app.helper import aggregator
 
-myApp = create_app()
+myCurrencyApp = create_app()
 
 
-@myApp.route('/get_currencies')
+@myCurrencyApp.route('/get_currencies')
 async def get_currencies():
     data = await aggregator()
     return jsonify(data)
 
 if __name__ == '__main__':
-    myApp.run(debug=True, host='localhost', port=5050)
+    myCurrencyApp.run(debug=True, host='localhost', port=5050)
