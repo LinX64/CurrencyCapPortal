6d417f86e02705b886491b504a597335732b4227
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-06-23 14:38:52 +0200

    Applying changes

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
 from flask import Flask, jsonify
 from app.helper import aggregator
 
-
-def create_app():
-    app = Flask(__name__)
-
-    @app.route('/get_currencies')
-    def get_currencies():
-        data = aggregator()  # Remove async/await unless you're using a Flask extension that supports it
-        return jsonify(data)
-
-    return app
+app = Flask(__name__)
 
 
-# This line is what Vercel will use
-app = create_app()
+@app.route('/get_currencies')
+def get_currencies():
+    data = aggregator()
+    return jsonify(data)
 
-# Keep this for local testing, Vercel will ignore it
-if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=5050)
+
+app = app
