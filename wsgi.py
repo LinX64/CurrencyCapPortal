b49414984a735b8e7cd2b8d25e0e2aede4b66b03
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-06-23 17:58:09 +0200

    Applying changes

--- wsgi.py
+++ wsgi.py
@@ -3,13 +3,13 @@ from flask import jsonify
 from app import create_app
 from app.helper import aggregator
 
-myCurrencyApp = create_app()
+app = create_app()
 
 
-@myCurrencyApp.route('/get_currencies')
+@app.route('/get_currencies')
 async def get_currencies():
     data = await aggregator()
     return jsonify(data)
 
 if __name__ == '__main__':
-    myCurrencyApp.run(debug=False, host='localhost', port=5050)
+    app.run(debug=False, host='localhost', port=5050)
