fda986cb981da473b204af7807c0f7909a7a88d2
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-06-30 10:55:55 +0200

    Added formatting for json

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import asyncio
 
 from flask import json
 
-from helper import aggregator, getBlockchainNews
+from helper import aggregator
 
 if __name__ == '__main__':
     data = asyncio.run(aggregator())
 
-    print(json.dumps(data))
+    print(json.dumps(data, indent=2))
