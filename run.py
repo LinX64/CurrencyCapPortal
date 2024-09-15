97e48745cf324390b6a7772806cd6cf5e5e2ee0b
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-09-15 21:50:16 +0200

    Running script with one line

--- run.py
+++ run.py
@@ -2,9 +2,11 @@ import asyncio
 
 from flask import json
 
-from helper import aggregator
+from helper import aggregator, getBlockchainNews
 
 if __name__ == '__main__':
+    blockchainNews = asyncio.run(getBlockchainNews())
     data = asyncio.run(aggregator())
 
+    print(json.dumps(blockchainNews, indent=2))
     print(json.dumps(data, indent=2))
