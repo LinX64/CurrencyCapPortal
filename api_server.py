5eb9e62faed0dc93ed67f4b50646f48ac282bccb
Mohsen Rzna <ash.wxrz@hotmail.com>
2025-11-20 22:49:27 +0100

    Fixes

--- api_server.py
+++ api_server.py
@@ -251,6 +251,5 @@ def health():
 
 
 if __name__ == '__main__':
-    # Use PORT environment variable for Cloud Run compatibility
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
